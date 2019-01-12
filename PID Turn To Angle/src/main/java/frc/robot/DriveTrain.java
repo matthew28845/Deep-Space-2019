4b6ef2ca74fed8302875d4f2494324919d848008
matthew28845 <matthew28845@yandex.com>
2019-01-12 10:33:47 -0600

    axfdsafrewdsrewdsxe

--- PID Turn To Angle/src/main/java/frc/robot/DriveTrain.java
+++ PID Turn To Angle/src/main/java/frc/robot/DriveTrain.java
@@ -7,8 +7,18 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * Add your docs here.
  */
 public class DriveTrain {
+    Talon talonZero = new Talon(0);
+    Talon talonOne = new Talon(1);
+    Talon talonTwo = new Talon(2);
+    Talon talonThree = new Talon(3);
+    public DriveTrain () {
+        
+    }
 }
